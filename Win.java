a7c7d5f06e0becc2eb8a63d1b6e660539fb93cf6
chloecas <chloe.castrataro@hotmail.com>
2025-04-10 17:37:59 -0400

    soundfile for win screen wasn't working, I removed, but will replace later

--- Win.java
+++ Win.java
@@ -18,7 +18,7 @@ public class Win extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 700, 1); 
         showTextWithBigBlackFont("You Win!", 180, 200);
-        gfs_GameOverWorld =  new  GreenfootSound("Win.wav");
+        gfs_GameOverWorld =  new  GreenfootSound("");
     }
     
     public void showTextWithBigBlackFont(String message, int x, int y)
