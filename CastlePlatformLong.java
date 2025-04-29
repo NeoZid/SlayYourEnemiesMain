898745cb1182adae37ce2c2d8ab6f08a1dfac5a3
Neo Zidereck De Guzman <2375650@edu.vaniercollege.qc.ca>
2025-04-29 12:43:03 -0400

    changed platform and resized few images

---
+++ CastlePlatformLong.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CastlePlatformLong here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CastlePlatformLong extends Ground
+{
+    /**
+     * Act - do whatever the CastlePlatformLong wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public CastlePlatformLong(){
+        
+    
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
